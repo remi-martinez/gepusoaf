292e3dcd2a1e2224d71f4e88166a2eb14540dfee
remi-martinez <remi.martinez74500@outlook.fr>
2022-01-05 14:42:52 +0100

    reorga dossier features

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
-import App from './components/app/App';
+import App from './features/components/app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
---
+++ server/src/main/java/com/gepusoaf/restservice/classe/ClasseService.java
@@ -0,0 +1,2 @@
+package com.gepusoaf.restservice.classe;public class ClasseService {
+}
