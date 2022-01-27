d95f294d3bc6b8e65549c73ed9e175b714d58c45
remi-martinez <remi.martinez74500@outlook.fr>
2022-01-27 02:25:49 +0100

    improvements in inscription.js

--- client/src/features/pages/help/Help.js
+++ client/src/features/pages/help/Help.js
@@ -16,20 +16,34 @@ function Help() {
                 <h3>Entreprise</h3>
                 <Accordion>
                     <Panel title="Comment rechercher une entreprise ?">
-                        Si vous voulez rechercher une entreprise, vous devez aller sur la page "Entreprise", pour cliquer sur le lien.<br/>
-                        Utilisez-les afin de pouvoir trouver les entreprises qui correspondent à vos choix.
+                        Si vous voulez rechercher une entreprise, vous devez aller sur la page " Entreprise ", pour cliquer sur
+                        le bouton " Rechercher une entreprise ". <br/>Il vous est alors fourni trois critères. Utilisez-les afin de
+                        pouvoir trouver les entreprises qui correspondent à vos choix.
                     </Panel>
                     <Panel title="Comment ajouter une entreprise ?">
-                        Pour ajouter une entreprise, rendez-vous sur la page "Entrepris", où vous devez cliquer sur le bouton concernant l'entreprise.<br/>
-                        Toutes les informations ne sont pas obligatoires, mais il est conseillé d'en fournir des référencées.
+                        Pour ajouter une entreprise, rendez-vous sur la page " Entreprise ", où vous devez cliquer sur le
+                        bouton " Ajouter une entreprise ". Vous devrez ensuite ajouter les informations concernant
+                        l’entreprise. <br/>Toutes les informations ne sont pas obligatoires, mais il est conseillé d’en fournir un
+                        maximum pour renseigner les futurs stagiaires sur les entreprises référencées.
                     </Panel>
                     <Panel title="Comment afficher ou enlever une information concernant l'entreprise ?">
-                        En allant sur la page "Entreprise", vous pouvez voir les entreprises déjà référencées.<br/>
-                        Vous pouvez alors les afficher grâce à la liste déroulante: choisissez l'information que vous voulez afficher,<br/>
-                        il vous suffit de cliquer sur le moins situé à l'entête de la colonne représentant l'information.
+                        En allant sur la page " Entreprise ", vous pouvez voir les entreprises déjà référencées. Vous pouvez
+                        alors remarquer que certaines informations concernant l'entreprise sont absentes. <br/>Vous pouvez
+                        cependant les afficher grâce à la liste déroulante : choisissez l'information que vous voulez afficher
+                        puis cliquez sur le bouton " Ajouter ". <br/>Si vous voulez enlever une information, il vous suffit de cliquer
+                        sur le moins situé à l'entête de la colonne représentant l'information concerné.
                     </Panel>
                     <Panel title="N'y a t-il pas une autre solution pour voir ces informations ?">
-                        Bien sûr, vous pouvez cliquer sur l'icône oeil pour voir toutes les informations l'entreprise sélectionnée.
+                        Bien sûr, vous pouvez cliquer sur l’icône correspondant pour voir toutes les informations concernant
+                        l'entreprise que vous avez sélectionné.
+                    </Panel>
+                    <Panel title="Comment puis-je supprimer une entreprise ?">
+                        Rien de plus simple, il vous suffit de cliquer sur l'icône correspondant qui se situe sur la deuxième colonne "
+                        Opération ".
+                    </Panel>
+                    <Panel title="Et si je veux modifier une information fausse ?">
+                        Cliquez sur l’icône correspondant, puis changer le(s) information(s) que vous voulez. Vous pourrez par la même
+                        occasion renseigner une information manquante si vous en avez la possibilité.
                     </Panel>
                 </Accordion>
             </div>
