f83d409f1a42a7e09c5ec2767adc2a5f5f6923d7
Aubert <bama73000@gmail.com>
2022-01-31 18:30:45 +0100

    Page d'ajout stagiaire

--- client/src/features/pages/student/StudentAddPage.js
+++ client/src/features/pages/student/StudentAddPage.js
@@ -1,12 +1,109 @@
 import React from 'react';
+import {Input} from "baseui/input";
+import {FlexGrid, FlexGridItem} from "baseui/flex-grid";
+import {Select} from "baseui/select";
+import {Button} from "baseui/button"; // add this package to your repo with `$ yarn add email-validator`
+
 
 function StudentAddPage() {
+    const [valueNom, setValueNom] = React.useState("");
+    const [valuePrenom, setValuePrenom] = React.useState("");
+    const [valueNomUtilisateur, setValueNomUtilisateur] = React.useState("");
+    const [valueMotDePasse, setValueMotDePasse] = React.useState("");
+    const [valueDateBTS, setValueDateBTS] = React.useState("");
+    const [valueClasse, setValueClasse] = React.useState("");
+    const itemProps = {
+        backgroundColor: 'mono300',
+        height: 'scale1000',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
 
     return (
         <>
-            <div>
-                hello student ajout
-            </div>
+            <form onSubmit={e => e.preventDefault()}>
+                <FlexGrid
+                    flexGridColumnCount={3}
+                    flexGridColumnGap="scale800"
+                    flexGridRowGap="scale800"
+                >
+                    <FlexGridItem>
+                        <label htmlFor="nom">Nom 📍</label>
+                        <Input
+                            {...itemProps}
+                            name="nom"
+                            value={valueNom}
+                            onChange={e => setValueNom(e.target.value)}
+                            type="text"
+                            placeholder="Doe"
+                            clearOnEscape
+                        />
+                    </FlexGridItem>
+                    <FlexGridItem>
+                        <label htmlFor="prenom">Prénom 📍</label>
+                        <Input
+                            {...itemProps}
+                            name="prenom"
+                            value={valuePrenom}
+                            onChange={e => setValuePrenom(e.target.value)}
+                            type="text"
+                            placeholder="John"
+                            clearOnEscape
+                        />
+                    </FlexGridItem>
+                    <FlexGridItem>
+                        <label htmlFor="nomUtilisateur">Nom d'utilisateur 📍</label>
+                        <Input
+                            {...itemProps}
+                            name="nomUtilisateur"
+                            value={valueNomUtilisateur}
+                            onChange={e => setValueNomUtilisateur(e.target.value)}
+                            type="text"
+                            placeholder="(8 caractères)"
+                            clearOnEscape
+                        />
+                    </FlexGridItem>
+                    <FlexGridItem >
+                        <label htmlFor="motDePasse">Mot de passe 📍</label>
+                        <Input
+                            {...itemProps}
+                            name="motDePasse"
+                            value={valueMotDePasse}
+                            onChange={e => setValueMotDePasse(e.target.value)}
+                            type="password"
+                            placeholder="(Entre 8 et 30 caractères)"
+                            clearOnEscape
+                        />
+                    </FlexGridItem>
+                    <FlexGridItem>
+                        <label htmlFor="dateBTS">Date d'obtention du BTS</label>
+                        <Input
+                            {...itemProps}
+                            name="dateBTS"
+                            value={valueDateBTS}
+                            onChange={e => setValueDateBTS(e.target.value)}
+                            type="Date"
+                            placeholder="Doe"
+                            clearOnEscape
+                        />
+                    </FlexGridItem>
+                    <FlexGridItem>
+                        <label htmlFor="classe">Classe 📍</label>
+                        <Select
+                            {...itemProps}
+                            name="classe"
+                            value={valueClasse}
+                            onChange={e => setValueClasse(e.target.value)}
+                            clearOnEscape
+                            required
+                        />
+                    </FlexGridItem>
+                </FlexGrid>
+
+            </form>
+            <p>Les champs avec le symbole 📍 sont obligatoires</p>
+            <Button style={{float: "right", marginRight: "10"}} type="submit">Ajouter</Button>
         </>
     );
 
